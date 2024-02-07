3514d500595ab98f2ba6beb8032c864c8f7fa066
46elks <46elks@46elkss-mbp.home>
2024-02-07 17:23:56 +0100

    Added 16x16 grid in javascript, Added mouseHover with random color effect

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const gridContainer = document.getElementById("grid-container")
+    const gridContainer = document.getElementById("grid-container");
 
     for (let i = 0; i < 256; i++) {
         let div = document.createElement('div');
-       gridContainer.appendChild(div);
-      }
+        gridContainer.appendChild(div);
+    }
+
+    gridContainer.addEventListener('mouseover', function(event) {
+        if (event.target.matches('div')) {
+            event.target.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
+    });
 });
